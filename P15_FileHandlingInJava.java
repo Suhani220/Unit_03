cdcd9ce1d4e709a5d86b4cc50ef635eaa9701f93
Suhani220 <102501106+Suhani220@users.noreply.github.com>
2022-05-29 23:38:45 +0530

    Add files via upload

---
+++ P15_FileHandlingInJava.java
@@ -0,0 +1,23 @@
+package Unit_03;
+import java.io.File;
+import java.io.FileReader;
+
+public class P15_FileHandlingInJava {
+	
+	public static void main(String[] args) {
+		
+		
+		
+	}
+}
+
+class ABCD{
+	
+	void takeInputFromFile() {
+		
+		String path = "C:\\Users\\Suhani\\Desktop";
+		File fl = new File(path);
+		
+	}
+	
+}
